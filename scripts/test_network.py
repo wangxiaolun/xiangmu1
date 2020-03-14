4f92adda6a8c9550cc7fea785f168ea9f8015991
WXL <431114697@qq.com>
2020-03-14 19:37:21 +0800

    增加7

--- scripts/test_network.py
+++ scripts/test_network.py
@@ -42,4 +42,11 @@ class TestNetwork:
         self.network_page.click_first_network()
         self.network_page.click_6g_network()
 
+    def test_mobile_network_7g(self):
+        self.network_page.click_more()
+        self.network_page.click_network()
+        self.network_page.click_first_network()
+        self.network_page.click_7g_network()
+
+
 

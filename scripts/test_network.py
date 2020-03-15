cd109aefb0ee8cea8fc89a032af08d115f4fe75d
WXL <431114697@qq.com>
2020-03-15 15:04:56 +0800

    增加了9G

--- scripts/test_network.py
+++ scripts/test_network.py
@@ -54,5 +54,11 @@ class TestNetwork:
         self.network_page.click_first_network()
         self.network_page.click_8g_network()
 
+    def test_mobile_network_9g(self):
+        self.network_page.click_more()
+        self.network_page.click_network()
+        self.network_page.click_first_network()
+        self.network_page.click_9g_network()
+
 
 
